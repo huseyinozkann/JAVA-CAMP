0cda2a2c8197a70c9ba97ea1751783ec1b5f4435
Hüseyin Özkan <ozkan.hsyn1@gmail.com>
2022-09-28 12:46:27 +0300

    week-1-project
    Week-1-project

---
+++ Work-1/sayiBulma/src/sayiBulma/Main.java
@@ -0,0 +1,22 @@
+package sayiBulma;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] sayilar = new int[] {1,2,3,4,5,7,9,0};
+		int aranacak = 6;
+		boolean varMı = false;
+		
+		for(int sayi : sayilar) {
+			if(sayi==aranacak) {
+				varMı = true;
+				break;
+			}
+		}
+		if(varMı) {
+		   System.out.println("Sayı mevcuttur");	
+		}else {
+			System.out.println("Sayı mevcut değildir");
+		}
+	}
+
+}
